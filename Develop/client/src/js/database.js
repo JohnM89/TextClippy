730eb6b5dc8823030a1b212784593ae2924b8532
JohnM89 <mjammer2@gmail.com>
2024-03-13 01:20:16 -0400

    implemented PUT and GET request logic for clients browser

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  console.log('PUT to the database:', content);
+  
+  const contactDb = await openDB('jate', 1);
+  
+  // ensure reading and writing to the database
+  const tx = contactDb.transaction('jate', 'readwrite');
+  
+  const store = tx.objectStore('jate');
+  
+  const request = store.put(content);
+  
+ 
+  await request;
+  
+  console.log('Content added/updated successfully.');
+};
+
+
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET from the database');
+  
+  const contactDb = await openDB('jate', 1);
+  
+  // read only transaction
+  const tx = contactDb.transaction('jate', 'readonly');
+  
+  const store = tx.objectStore('jate');
+  
+  const request = store.getAll();
+  
+  const result = await request;
+  
+  console.log('result.value', result);
+  
+  return result;
+};
 
 initdb();
