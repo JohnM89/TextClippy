e2ccafcf2833e78b9618e1389ff7d1d07e99f947
JohnM89 <mjammer2@gmail.com>
2024-03-13 03:21:04 -0400

    fixed getDb function data was not being pulled correctly

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,45 +12,37 @@ const initdb = async () =>
     },
   });
 
-
+// putDb function
 export const putDb = async (content) => {
   console.error('putDb not implemented');
   console.log('PUT to the database:', content);
-  
   const contactDb = await openDB('jate', 1);
   
   // ensure reading and writing to the database
   const tx = contactDb.transaction('jate', 'readwrite');
-  
   const store = tx.objectStore('jate');
-  
   const request = store.put(content);
-  
- 
   await request;
-  
   console.log('Content added/updated successfully.');
 };
 
 
+// getDb function
 export const getDb = async () => {
-  console.error('getDb not implemented');
-  console.log('GET from the database');
-  
-  const contactDb = await openDB('jate', 1);
-  
-  // read only transaction
-  const tx = contactDb.transaction('jate', 'readonly');
-  
-  const store = tx.objectStore('jate');
-  
-  const request = store.getAll();
-  
-  const result = await request;
-  
-  console.log('result.value', result);
-  
-  return result;
+  try {
+    console.log('Retrieving...');
+    const jateDb = await openDB('jate', 1);
+    const transaction = jateDb.transaction('jate', 'readonly');
+    const safe = transaction.objectStore('jate');
+    // get the first item in the database (was missing initially)
+    const request = safe.get(1); 
+    const result = await request;
+    console.log('Retrieved content:', result);
+    return result?.value; 
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return null;
+  }
 };
 
 initdb();
--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -1,36 +1,41 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
 
 export default class {
   constructor() {
-    // Retrieve data from IndexedDB or localStorage using getDb
+    const localData = localStorage.getItem('content');
+
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
+
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
+
+    // When the editor is ready, set the value to whatever is stored in indexeddb.
+    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      // Combine header content with retrieved data or set to header if no data is retrieved
-      const initialContent = data ? header + '\n' + data : header;
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    });
 
-      // Initialize CodeMirror editor with the combined content
-      this.editor = CodeMirror(document.querySelector('#main'), {
-        value: initialContent,
-        mode: 'javascript',
-        theme: 'monokai',
-        lineNumbers: true,
-        lineWrapping: true,
-        autofocus: true,
-        indentUnit: 2,
-        tabSize: 2,
-      });
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
 
-      // Event listener for editor changes to update localStorage
-      this.editor.on('change', () => {
-        localStorage.setItem('content', this.editor.getValue());
-      });
-
-      // Event listener to save content to IndexedDB when editor loses focus
-      this.editor.on('blur', () => {
-        console.log('The editor has lost focus');
-        putDb(localStorage.getItem('content'));
-      });
+    // Save the content of the editor when the editor itself is loses focus
+    this.editor.on('blur', () => {
+      console.log('The editor has lost focus');
+      putDb(localStorage.getItem('content'));
     });
   }
 }
