ea724d1a3d7d87490ccf83612901f4e58075b688
JohnM89 <mjammer2@gmail.com>
2024-03-13 01:48:49 -0400

    fun stylings (favicon/ascii/.png)

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,7 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
     
@@ -30,6 +29,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
+    console.log('PWA was installed', event);
   window.deferredPrompt = null;
 }); 
